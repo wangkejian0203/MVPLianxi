16eae87f762ed283fae46fd06b7a90e50f18e404
社会王 <3208844611@qq.com>
2019-02-15 18:15:53 +0800

    Initial commit

---
+++ LogintoRegister/src/main/java/com/example/logintoregister/ABc.java
@@ -0,0 +1,15 @@
+package com.example.logintoregister;
+
+/**
+ * 2 * Copyright (C), 2015-2019, XXX有限公司
+ * 3 * FileName: ABc
+ * 4 * Author: 社会王
+ * 5 * Date: 2019.02.15 17:13
+ * 6 * Description: aa
+ * 7 * History:44444
+ * 8 * <author> <time> <version> <desc>
+ * 9 * 作者姓名 修改时间 版本号 描述
+ * 10
+ */
+public class ABc {
+}
---
+++ LogintoRegister/src/main/java/com/example/logintoregister/DAa.java
@@ -0,0 +1,12 @@
+package com.example.logintoregister;
+
+/**
+ * 2 * Copyright (C), 2015-2019, XXX有限公司
+ * 3 * FileName: DAa
+ * 4 * Author: 王科健
+ * 5 * Date: 2019.02.15 17:16
+ * 6 * Description: a
+ * 10
+ */
+public class DAa {
+}
